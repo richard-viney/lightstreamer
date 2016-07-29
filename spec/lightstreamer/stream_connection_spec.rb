082710c117f3371ae10e70269eb669bb7c0d3e9e
Richard Viney <richard.viney@gmail.com>
2016-07-29 23:19:13 +1200

    Fixed spec errors

--- spec/lightstreamer/stream_connection_spec.rb
+++ spec/lightstreamer/stream_connection_spec.rb
@@ -6,7 +6,7 @@ describe Lightstreamer::StreamConnection do
 
   let(:create_args) do
     query = { LS_op2: 'create', LS_cid: 'mgQkwtwdysogQz2BJ4Ji kOj2Bg', LS_user: 'username', LS_password: 'password',
-              LS_adapter_set: 'set' }
+              LS_adapter_set: 'set', LS_report_info: true }
 
     ['http://test.com/lightstreamer/create_session.txt', hash_including(query: query, connect_timeout: 15)]
   end
@@ -41,7 +41,7 @@ describe Lightstreamer::StreamConnection do
     end
 
     bind_args = ['http://a.com/lightstreamer/bind_session.txt',
-                 hash_including(query: { LS_session: 'A' }, connect_timeout: 15)]
+                 hash_including(query: { LS_session: 'A', LS_report_info: true }, connect_timeout: 15)]
     expect(Excon).to receive(:post).with(*bind_args) do |_url, params|
       params[:response_block].call "OK\r\nSessionId:A\r\nControlAddress:a.com\r\n\r\nthree\r\nfour\r\n", nil, nil
       sleep
